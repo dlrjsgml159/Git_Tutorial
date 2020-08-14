0120da30aa0ff55d6b17f6a8c6fea9ce05727ff3
lee <gunhee260@gmail.com>
2020-08-14 18:24:02 +0900

    Add UserController[Update]

--- UserController.java
+++ UserController.java
@@ -16,4 +16,12 @@ pubilc class UserController{
 
 	return "user/insert";
   }
+
+  //회원 수정 페이지로 이동
+  @RequestMapping("/user/update.do")
+  public String update(){
+	logger.info("경로:update");
+
+	return "user/update";
+  } 
 }
